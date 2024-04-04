c0638f32eac059f7de754faee2a96ece21c4f8c5
Guilherme Simonato <guilhermesimonato4@gmail.com>
2024-04-04 16:53:14 -0300

    First commit

---
+++ components/link-button.js
@@ -0,0 +1,35 @@
+class Link extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' })
+    }
+
+    connectedCallback() {
+        const type = this.getAttribute('type')
+        const link = this.getAttribute('link') ?? "#"
+
+        if (type === 'closed') {
+            this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="./components/link/style.css">
+                <li class="disabled">
+                    <slot></slot>
+                    <ion-icon name="lock-closed-outline"></ion-icon> 
+                </li>
+            `
+            return;
+        }
+
+        this.shadowRoot.innerHTML = `
+            <link rel="stylesheet" href="./components/link/style.css">
+            <a href="${link}" target="_blank">
+            <li>
+                <slot></slot>
+                <ion-icon class="r-45" name="arrow-forward-outline"></ion-icon> 
+            </li>
+            </a>
+        `
+        this.shadowRoot.appendChild = '<link rel="stylesheet" href="./style/global.css">'
+    }
+}
+
+customElements.define('ds-link', Link)
